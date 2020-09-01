1230712c0494ea28e09698fd224a0db8d773eb74
Andree Velasco <apandcan@gmail.com>
2020-09-01 16:12:21 -0500

    Agregar archivos de proyecto.

---
+++ MVC_Parte5_AnotacionData/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using MVC_Parte5_AnotacionData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Parte5_AnotacionData.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DatosPersonales(ClsDatos obj)
+        {
+            if(ModelState.IsValid)
+            {
+                return View(obj);
+            }
+            else
+            {
+                return View("Index");
+            }           
+        }
+    }
+}
---
+++ MVC_Parte5_AnotacionData/Models/ClsDatos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Parte5_AnotacionData.Models
+{
+    public class ClsDatos
+    {
+        [Required(ErrorMessage ="El siguiente dato es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El siguiente dato es requerido")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "El siguiente dato es requerido")]
+        public string Edad { get; set; }
+    }
+}
